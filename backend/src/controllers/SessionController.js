1f4c1712fec2e104ac12f380583acb9603550b05
RafaAudibert - Deepin <rafaeelaudibert@gmail.com>
2019-10-04 00:51:32 -0300

    :sparkles: Added back-end

---
+++ backend/src/controllers/SessionController.js
@@ -0,0 +1,16 @@
+//index, show, create, update, destroy
+
+const User = require("../models/User");
+
+module.exports = {
+  async create(req, res) {
+    const { email } = req.body;
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      user = await User.create({ email });
+    }
+
+    return res.json(user);
+  }
+};
